acae16d88c3ae8d8930ee4a5a7447643f4cdda18
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2022-09-01 17:52:12 +0530

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -19,10 +19,10 @@ const Projects = (props) => {
             {props.myprojects.data.map((item)=>{
                 return (
                     <>
-                    <div className="p-4 w-1/3 ">
-              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden shadow-xl shadow-blue-800  cursor-pointer hover:scale-105 ease-in  duration-300  p-2 bg-[#ecf0f3] align-middle items-center ml-auto">
+                    <div className="p-4 w-full sm:w-1/3">
+              <div className=" h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden shadow-xl shadow-blue-800  cursor-pointer hover:scale-105 ease-in  duration-300  p-2 bg-[#ecf0f3] align-middle items-center ml-auto">
                 {/* <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"> */}
-                <div className="imagefolder flex align-middle justify-center "><Image
+                <div className="imagefolder flex align-middle justify-center  "><Image
                   src={item.attributes.imageurl}
                   alt={item.attributes.title}
                   width="200"
