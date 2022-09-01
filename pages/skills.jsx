c88afee512eabebb720261e640caec8dc182e9a8
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2022-09-01 16:40:37 +0530

    updating skills section

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import Logo from "../public/assets/logo.jpg";
-import RJs from "../public/assets/react.png";
+
 import { fetcher } from "../lib/api";
 
 
@@ -54,29 +53,31 @@ const Skills = (props) => {
             </section>
             <h2 className="flex justify-center ">Frameworks</h2>
             <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-               
-                
-               
-                <div className="w-full bg-white rounded-lg sahdow-lg  flex flex-col justify-center items-center  shadow-xl shadow-black  cursor-pointer hover:scale-105 ease-in  duration-300 p-2">
-                  <div className="mb-8">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {props.framework.data.map((item)=>{
+                return (
+                  <>
+                  <div className="w-full bg-white rounded-lg shadow-lg  flex flex-col justify-center items-center  shadow-black  cursor-pointer hover:scale-105 ease-in  duration-300 p-2">
+                  <div className="mb-8 " >
                     <Image
-                      src={RJs}
-                      alt="/"
-                      width="50"
-                      height="50"
-                      className="cursor-pointer rounded-full"
+                      src={item.attributes.imageurl}
+                      alt={item.attributes.name}
+                      width="100"
+                      height="100"
+                      objectFit='contain'
+                      className="rounded-full object-cover items-center flex justify-center  "
                     />
                   </div>
                   <div className="text-center">
-                    <p className="text-xl text-black font-bold mb-2">React</p>
-                    <p className="text-base text-gray-400 font-normal">
-                      ________________________________________
-                    </p>
+                    <p className="text-xl text-black font-bold mb-2">{item.attributes.name}</p>
+                    
                   </div>
                 </div>
-
-                
+                </>
+                )
+              })}
+              
+               
               </div>
             </section>
           </div>
@@ -89,11 +90,12 @@ const Skills = (props) => {
 export default Skills;
 export async function getStaticProps(){
   const frontend = await fetcher(`http://localhost:1337/api/frontends`);
-  const framework = await fetcher(`http://localhost:1337/api/framework`)
- 
+  const framework = await fetcher(`http://localhost:1337/api/frameworks`)
+//  console.log(framework.data)
   return{
       props:{
         frontend:frontend,
+        framework:framework,
       },
   }
 }
